7d2fecd9682f3d8b114b482d03cb8a196bcbaa82
mehtak1 <mehtak1@mcmaster.ca>
2019-03-26 22:19:06 -0400

    Add project files.

---
+++ JainFoodApp/www/scripts/GoogleMap.js
@@ -0,0 +1,137 @@
+﻿function initMap() {
+    var map = new google.maps.Map(
+        document.getElementById('map'),
+        {
+            center: { lat: 41.84, lng: -39.08 },
+            zoom: 3,
+            disableDefaultUI: true,
+            styles: [
+                {
+                    featureType: 'poi.business',
+                    stylers: [{ visibility: 'off' }]
+                },
+                {
+                    featureType: 'poi.school',
+                    stylers: [{ visibility: 'off' }]
+                },
+                {
+                    featureType: 'poi.sports_complex',
+                    stylers: [{ visibility: 'off' }]
+                },
+                {
+                    featureType: 'poi.medical',
+                    stylers: [{ visibility: 'off' }]
+                },
+                {
+                    featureType: 'poi.place_of_worship',
+                    stylers: [{ visibility: 'off' }]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'labels.icon',
+                    stylers: [{ visibility: 'off' }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#fafafa' }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#cc6c08' }]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#9ca5b3' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{ color: '#e5c4a0' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{ color: '#fff2af' }]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{ color: '#9ca5b3' }]
+                },
+            ]
+        });
+
+    var input = document.getElementById('pac-input');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.bindTo('bounds', map);
+
+    // Specify just the place data fields that you need.
+    autocomplete.setFields(['place_id', 'geometry', 'name', 'formatted_address']);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var infowindow = new google.maps.InfoWindow;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            map.setCenter(pos);
+            map.setZoom(11);
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        console.log("no support");
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+    }
+
+    var infowindowContent = document.getElementById('infowindow-content');
+    infowindow.setContent(infowindowContent);
+
+    var geocoder = new google.maps.Geocoder;
+
+    var marker = new google.maps.Marker({ map: map });
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+
+    autocomplete.addListener('place_changed', function () {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+
+        if (!place.place_id) {
+            return;
+        }
+        geocoder.geocode({ 'placeId': place.place_id }, function (results, status) {
+            if (status !== 'OK') {
+                window.alert('Geocoder failed due to: ' + status);
+                return;
+            }
+
+            map.setZoom(18);
+            map.setCenter(results[0].geometry.location);
+
+            // Set the position of the marker using the place ID and location.
+            marker.setPlace(
+                { placeId: place.place_id, location: results[0].geometry.location });
+
+            marker.setVisible(true);
+
+            infowindowContent.children['place-name'].textContent = place.name;
+            infowindowContent.children['place-address'].textContent =
+                results[0].formatted_address;
+
+            infowindow.open(map, marker);
+        });
+    });
+}
